ef8d9eab7c021badbb33ad4af0af0e14808c82f9
Kol.ds <fatbahad@gmail.com>
2024-10-04 22:46:27 +0300

    add Camera Pawn

---
+++ Source/TDRussVsLizzards/Private/Camera/CameraPawn.cpp
@@ -0,0 +1,34 @@
+// TD Russ Vs Lizzards Game
+
+#include "Camera/CameraPawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ACameraPawn::ACameraPawn()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    SceneComponent = CreateDefaultSubobject<USceneComponent>("SceneComponent");
+    SetRootComponent(SceneComponent);
+
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+    CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void ACameraPawn::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ACameraPawn::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ACameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/TDRussVsLizzards/Public/Camera/CameraPawn.h
@@ -0,0 +1,41 @@
+// TD Russ Vs Lizzards Game
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CameraPawn.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class USceneComponent;
+
+UCLASS()
+class TDRUSSVSLIZZARDS_API ACameraPawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    ACameraPawn();
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+    virtual void BeginPlay() override;
+
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USceneComponent* SceneComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USpringArmComponent* SpringArmComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UCameraComponent* CameraComponent;
+
+    
+
+private:
+
+
+};
