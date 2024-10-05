6600ce0504913847410e36aa7efe858c2f3975fa
Kol.ds <fatbahad@gmail.com>
2024-10-05 22:50:25 +0300

    fix input

--- Source/TDRussVsLizzards/Private/Camera/TDCameraController.cpp
+++ Source/TDRussVsLizzards/Private/Camera/TDCameraController.cpp
@@ -2,7 +2,62 @@
 
 
 #include "Camera/TDCameraController.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "EnhancedInputSubsystems.h"
 
 ATDCameraController::ATDCameraController() {}
+
+void ATDCameraController::BeginPlay() 
+{
+    Super::BeginPlay();
+}
+
+void ATDCameraController::SetupInputComponent() 
+{
+    Super::SetupInputComponent();
+
+    // Add Input Mapping Context
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+    {
+        Subsystem->AddMappingContext(DefaultMappingContext, 0);
+    }
+
+    // Set up action bindings
+    if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+    {
+        EnhancedInputComponent->BindAction(MoveCameraUpAction, ETriggerEvent::Triggered, this, &ATDCameraController::MoveCameraUp);
+        EnhancedInputComponent->BindAction(MoveCameraDownAction, ETriggerEvent::Triggered, this, &ATDCameraController::MoveCameraDown);
+        EnhancedInputComponent->BindAction(MoveCameraRightAction, ETriggerEvent::Triggered, this, &ATDCameraController::MoveCameraRight);
+        EnhancedInputComponent->BindAction(MoveCameraLeftAction, ETriggerEvent::Triggered, this, &ATDCameraController::MoveCameraLeft);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error,
+            TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend "
+                 "to use the legacy system, then you will need to update this C++ file."),
+            *GetNameSafe(this));
+    }
+}
+
+void ATDCameraController::MoveCameraUp() 
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("UP"));
+}
+
+void ATDCameraController::MoveCameraDown() 
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("DOWN"));
+}
+
+void ATDCameraController::MoveCameraRight() 
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("RIGHT"));
+}
+
+void ATDCameraController::MoveCameraLeft()
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("LEFT"));
+}
 
 
--- Source/TDRussVsLizzards/Public/Camera/TDCameraController.h
+++ Source/TDRussVsLizzards/Public/Camera/TDCameraController.h
@@ -6,9 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "TDCameraController.generated.h"
 
-/**
- *
- */
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class TDRUSSVSLIZZARDS_API ATDCameraController : public APlayerController
 {
@@ -17,7 +17,30 @@ class TDRUSSVSLIZZARDS_API ATDCameraController : public APlayerController
 public:
     ATDCameraController();
 
+protected:
+    virtual void BeginPlay() override;
+    virtual void SetupInputComponent() override;
+
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    UInputMappingContext* DefaultMappingContext;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    UInputAction* MoveCameraUpAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    UInputAction* MoveCameraDownAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    UInputAction* MoveCameraRightAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+    UInputAction* MoveCameraLeftAction;
+
+
 private:
-
-
+    void MoveCameraUp();
+    void MoveCameraDown();
+    void MoveCameraRight();
+    void MoveCameraLeft();
 };
