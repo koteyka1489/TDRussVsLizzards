30853e692f74461561e33ad8fe48fc1746c59bab
Kol.ds <fatbahad@gmail.com>
2024-10-09 16:50:31 +0300

    need fix pawn movement and gravity

--- Source/TDRussVsLizzards/Private/Creeps/BaseCreepPawn.cpp
+++ Source/TDRussVsLizzards/Private/Creeps/BaseCreepPawn.cpp
@@ -2,26 +2,43 @@
 
 #include "Creeps/BaseCreepPawn.h"
 #include "Components/HealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Goal/TDGoal.h"
+#include "AI/TDAIController.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "Components/CapsuleComponent.h"
 
 ABaseCreepPawn::ABaseCreepPawn()
 {
     PrimaryActorTick.bCanEverTick = true;
 
+    CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>("CapsuleComponent");
+    SetRootComponent(CapsuleComponent);
+
     SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>("SkeletalMeshComponent");
-    SetRootComponent(SkeletalMeshComponent);
+    SkeletalMeshComponent->SetupAttachment(CapsuleComponent);
 
     HealthComponent = CreateDefaultSubobject<UHealthComponent>("HealthComponent");
-
 }
 
 void ABaseCreepPawn::BeginPlay()
 {
     Super::BeginPlay();
+
+    Goal = Cast<ATDGoal>(UGameplayStatics::GetActorOfClass(GetWorld(), ATDGoal::StaticClass()));
+    if (!Goal)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Goal Cast Failed"));
+        checkNoEntry();
+    }
 }
 
 void ABaseCreepPawn::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    FVector Direction = (Goal->GetActorLocation() - GetActorLocation()).GetSafeNormal();
+    AddMovementInput(Direction, 1.0f);
 }
 
 void ABaseCreepPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- Source/TDRussVsLizzards/Public/Creeps/BaseCreepPawn.h
+++ Source/TDRussVsLizzards/Public/Creeps/BaseCreepPawn.h
@@ -8,6 +8,8 @@
 
 class UHealthComponent;
 class USkeletalMeshComponent;
+class ATDGoal;
+class UCapsuleComponent;
 
 UCLASS()
 class TDRUSSVSLIZZARDS_API ABaseCreepPawn : public APawn
@@ -23,10 +25,15 @@ protected:
     virtual void BeginPlay() override;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+    UCapsuleComponent* CapsuleComponent;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
     USkeletalMeshComponent* SkeletalMeshComponent;
 
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
     UHealthComponent* HealthComponent;
 
 private:
+    TObjectPtr<ATDGoal> Goal;
+    
 };
