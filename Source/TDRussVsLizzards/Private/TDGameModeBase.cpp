0aa709c9dc9ed425e1e470a48af42034d836a227
Kol.ds <fatbahad@gmail.com>
2024-10-10 12:50:16 +0300

    add  base creeps spawn usung startpoints

--- Source/TDRussVsLizzards/Private/TDGameModeBase.cpp
+++ Source/TDRussVsLizzards/Private/TDGameModeBase.cpp
@@ -1,12 +1,50 @@
 // TD Russ Vs Lizzards Game
 
-
 #include "TDGameModeBase.h"
 #include "Camera/TDCameraController.h"
 #include "Camera/CameraPawn.h"
+#include "Creeps/BaseCreepPawn.h"
+#include "AI/TDAIController.h"
 
-ATDGameModeBase::ATDGameModeBase() 
+ATDGameModeBase::ATDGameModeBase()
 {
-    DefaultPawnClass = ACameraPawn::StaticClass();
+    DefaultPawnClass      = ACameraPawn::StaticClass();
     PlayerControllerClass = ATDCameraController::StaticClass();
+}
+
+void ATDGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+    SpawnCreeps();
+}
+
+UClass* ATDGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return CreepPawn;
+    }
+
+
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ATDGameModeBase::SpawnCreeps()
+{
+    int32 CreepsRows = GameData.CreepsNum / CreepsInRow;
+    FActorSpawnParameters SpawnInfo;
+    SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+
+    for (int32 y = 0; y < CreepsRows; y++)
+    {
+        for (int32 x = 0; x < CreepsInRow; x++)
+        {
+            FRotator BaseRotation = FRotator::ZeroRotator;
+            FVector BaseLocation  = FVector(50.0 * (double)x, 50.0 * (double)y, 0.0);
+
+            const auto CreepAiController = GetWorld()->SpawnActor<AAIController>(CreepController, SpawnInfo);
+            RestartPlayer(CreepAiController);
+        }
+    }
 }
--- Source/TDRussVsLizzards/Public/TDGameModeBase.h
+++ Source/TDRussVsLizzards/Public/TDGameModeBase.h
@@ -6,9 +6,19 @@
 #include "GameFramework/GameModeBase.h"
 #include "TDGameModeBase.generated.h"
 
-/**
- *
- */
+class ABaseCreepPawn;
+class AAIController;
+
+
+USTRUCT(BlueprintType)
+struct FGameData
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Creeps", meta = (ClampMin = "5", ClampMax = "1000"))
+    int32 CreepsNum = 50;
+};
+
 UCLASS()
 class TDRUSSVSLIZZARDS_API ATDGameModeBase : public AGameModeBase
 {
@@ -16,4 +26,29 @@ class TDRUSSVSLIZZARDS_API ATDGameModeBase : public AGameModeBase
 
 public:
     ATDGameModeBase();
+    virtual void StartPlay() override;
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+protected:
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SpawnCreeps")
+    FGameData GameData;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SpawnCreeps")
+    TSubclassOf<AAIController> CreepController;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SpawnCreeps")
+    TSubclassOf<ABaseCreepPawn> CreepPawn;
+
+
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SpawnCreeps")
+    int32 CreepsInRow = 10;
+
+
+private:
+    TArray<TObjectPtr<ABaseCreepPawn>> SpawnedCrepps;
+
+
+    void SpawnCreeps();
 };
